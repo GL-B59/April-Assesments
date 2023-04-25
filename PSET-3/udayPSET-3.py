a06d9fff2275d1b39d24b41814174144ce46db2d
Uday <udaynani277@gmail.com>
2023-04-25 08:18:37 +0530

    Solved udayPSET-3.py

---
+++ PSET-3/udayPSET-3.py
@@ -0,0 +1,9 @@
+#Solution for PSET-3
+string_one=input()
+whitespace=string_one.find(" ")
+first_name=string_one[:whitespace]
+last_name=string_one[whitespace+1:]
+index_last_name=whitespace+1
+index_first_name=whitespace-1
+print(index_last_name)
+print(index_first_name)
