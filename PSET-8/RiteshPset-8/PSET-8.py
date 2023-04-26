5222d3ec1cdad0135df3fc1e6ae462a75067d6c2
Ritesh <shenderitesh3@gmail.com>
2023-04-26 23:57:58 +0530

    Added Pset-8 Solution

---
+++ PSET-8/RiteshPset-8/PSET-8.py
@@ -0,0 +1,9 @@
+#pset 8
+user_input = input("Enter the string:")
+a = user_input
+b = a.replace('a', 'A')
+c = b.replace('e', 'E')
+d = c.replace('i', 'I')
+e = d.replace('o', 'O')
+f = e.replace('u', 'U')
+print(f)
