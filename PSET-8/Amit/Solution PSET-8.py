8864597325856c35be37dc5243093886f49deddb
Amit karn <amitkmrkarn@gmail.com>
2023-04-27 20:05:10 +0530

    add Solution PSET-8

---
+++ PSET-8/Amit/Solution PSET-8.py
@@ -0,0 +1,6 @@
+#solution for PSET-8
+user_input=input("Enter some text:")
+vowel=vowel.replace('e', 'E')
+vowel=vowel.replace('i', 'I')
+vowel=vowel.replace('o', 'O')
+vowel=vowel.replace('u', 'U')
