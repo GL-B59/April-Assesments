69e6fc66c1a4015606b9f046169400263bdd777b
shivani121794 <shivani121794@gmail.com>
2023-04-27 12:17:57 +0530

    add Solution_PSET-8

---
+++ PSET-8/Shivani/Solution_PSET-8.py
@@ -0,0 +1,6 @@
+user_input=input("please enter your input:")
+vowel = user_input.replace('a','A').replace('e','E').replace('i','I').replace('o','O').replace('u','U')
+print(vowel)
+
+# Answer: please enter your input: I have some work today example our assignment.
+        # I hAvE sOmE wOrk tOdAy ExAmplE OUr AssIgnmEnt.
