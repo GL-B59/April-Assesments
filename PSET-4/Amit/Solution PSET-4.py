e88cddefa822b030f2121324d7cdbc93be2ca040
Amit karn <amitkmrkarn@gmail.com>
2023-04-27 19:44:04 +0530

    add Solution PSET-4

---
+++ PSET-4/Amit/Solution PSET-4.py
@@ -0,0 +1,11 @@
+#Solution for PSET-4
+
+#total_apple= 10
+#apple_eaten= 4
+#print('There are total_apple - apple_eaten + left in the basket.')
+Typeerror: It can only concatenate str to str
+# so converting int to string and the code is given below:
+
+total_apple= 10
+apple_eaten= 4
+print('there are' + str(total_apple-apple_eaten) + 'left in the basket.')
