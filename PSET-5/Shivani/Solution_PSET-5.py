a9719691a2c529af825dd3d2eb7a3322a4ee4d39
shivani121794 <shivani121794@gmail.com>
2023-04-27 11:57:54 +0530

    add Solution_PSET-5

---
+++ PSET-5/Shivani/Solution_PSET-5.py
@@ -0,0 +1,5 @@
+total_apple = 10
+apple_eaten = 4
+
+print(f'There are {total_apple - apple_eaten} left in the basket.') # replace the concatenation with an f-string (Cont of PSET-4)
+# Answer: There are 6 left in the basket.
